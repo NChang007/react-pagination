796d165efe6d7ac93e1c84ed0fe41bfb6d432c6b
Nelson Chang <nelsonc924@gmail.com>
2023-10-04 17:05:25 -0400

    css updates

--- src/pagination/Pagination.jsx
+++ src/pagination/Pagination.jsx
@@ -1,39 +1,40 @@
 import React from 'react'
+import './pagination.css'
 
 const Pagination = ({nPages, currentPage, setCurrentPage}) => {
     const pageNumbers = [...Array(nPages + 1).keys()].slice(1)
     const nextPage = () => {
-        if(currentPage !== nPages) 
-            setCurrentPage(currentPage + 1)
+        if(currentPage !== nPages) setCurrentPage(currentPage + 1)
     }
     const prevPage = () => {
-        if(currentPage !== 1) 
-            setCurrentPage(currentPage - 1)
+        if(currentPage !== 1) setCurrentPage(currentPage - 1)
     }
     return (
-        <nav aria-label="Page navigation example" className='pagination'>
-            <ul className="pagination">
-                <li className="page-item">
-                    <a className="page-link" href="#" onClick={prevPage}>Previous</a>
+        <nav aria-label="Page navigation" role='pagination' className='pagination'>
+            <ul className="pagination-list">
+                <li className="page-item prev" onClick={prevPage}>
+                    <a className="page-link" href="#" >Previous</a>
                 </li>
 
                 {pageNumbers.map(pageNum => {
                     return (
-                        <li 
-                            key={pageNum} 
-                            className={`page-item ${currentPage == pageNum ? 'active' : ''} pageNum`} 
+                        <li
+                            key={pageNum}
+                            className={`page-item ${currentPage == pageNum ? 'active' : ''} pageNum`}
+                            onClick={() => setCurrentPage(pageNum)}
                         >
-                            <a onClick={() => setCurrentPage(pageNum)}
-                            className="page-link" 
-                            href="#">
+                            <a
+                                className="page-link" 
+                                href="#"
+                            >
                                 {pageNum}
                             </a>
                         </li>
                     )
                 })}
 
-                <li className="page-item">
-                    <a className="page-link" href="#" onClick={nextPage}>Next</a>
+                <li className="page-item next" onClick={nextPage}>
+                    <a className="page-link" href="#" >Next</a>
                 </li>
             </ul>
         </nav>
